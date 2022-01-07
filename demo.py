75dde51f14e0d65fe8141491438ce3cf0c4b7732
tareefroustom <58273854+tareefroustom@users.noreply.github.com>
2022-01-07 13:16:15 +0100

    Update demo.py

--- demo.py
+++ demo.py
@@ -11,10 +11,10 @@ st.markdown("""<head>
                 </style>
                 </head>""", unsafe_allow_html=True)
 
-st.image("./logo.png")
+st.image("logo.png")
 st.markdown("<h1 style='text-align: center;font-size:40px; color:#676FA3; font-family: Titillium Web'>MIMAS Document Analysis and Management Demo</h1>", unsafe_allow_html=True)
 
-df = pd.read_excel("./BR_Title_Mapping_20220104.xlsx")
+df = pd.read_excel("BR_Title_Mapping_20220104.xlsx")
 pr = df.profile_report()
 
 st_profile_report(pr)
